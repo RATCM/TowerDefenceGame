cf0f6be1f3f91db8d7a8bf75ec598f667cc75528
RATCM <isakbjoershol@gmail.com>
2022-09-23 13:22:26 +0200

    More boilerplate

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class SceneLoader
+{
+    static bool LoadScene(string name)
+    {
+        bool sucess = true;
+        try
+        {
+            SceneManager.LoadScene(name);
+
+        }
+        catch (UnityException)
+        {
+            sucess = false;
+        }
+
+        return sucess;
+    }
+}
