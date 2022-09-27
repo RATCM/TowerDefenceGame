73919ab4eab6aa51580142aba8d75b9a0afbad88
abxh <83485102+abxh@users.noreply.github.com>
2022-09-27 13:06:07 +0200

    new towers

---
+++ Assets/Scripts/Global.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public static class Global
+{
+    public static GameObject SelectedTower;
+    public static string PointerState;
+}
