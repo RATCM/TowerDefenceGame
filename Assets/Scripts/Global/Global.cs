1a215e587c445f9c35f7972e23b3bdcfcbf77921
abxh <83485102+abxh@users.noreply.github.com>
2022-11-01 23:02:40 +0100

    a

--- Assets/Scripts/Global/Global.cs
+++ Assets/Scripts/Global/Global.cs
@@ -11,7 +11,7 @@ public static class Global
     public static string PointerState;
     public static bool RoundInProgress = false;
     public static List<Vector2> SpawnLocations = new List<Vector2>() { new Vector2(-10,0.5f) };
-    public static int MaxRounds = 9;
+    public static int MaxRounds = 5;
 
     public static void EndRound()
     {
